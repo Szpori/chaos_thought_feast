d9966ed13a9f1d0c55feb64aa790f6a0fd953b20
TrudnaCaptcha <36775976+Retardeded@users.noreply.github.com>
2024-07-09 11:34:36 +0200

    more scalable AppStrings class

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,48 +1,32 @@
 class AppStrings {
-  static const chaosThoughtFeast = 'Chaos Thought Feast';
-  static const findYourLikings = 'Find Your Likings';
-  static const likingSpectrumJourney = 'Liking Spectrum Journey';
-  static const anyfinCanHappen = 'Anyfin Can Happen';
-  static const profile = 'Profile';
+  static const String defaultLanguage = 'en';
 
-  static const chaosThoughtFeastPl = 'Chaotyczna Uczta Myśli';
-  static const findYourLikingsPl = 'Znajdź Swoje Upodobania';
-  static const likingSpectrumJourneyPl = 'Podróż przez Spektrum Upodobań';
-  static const anyfinCanHappenPl = 'Wszystko może się Zdarzyć';
-  static const profilePl = 'Profil';
-
-  static const selectCategory = 'Select a category';
-  static const startingConcept = 'Starting Concept';
-  static const goalConcept = 'Goal Concept';
-  static const startGame = 'Start Game';
-
-  static const selectCategoryPl = 'Wybierz kategorię';
-  static const startingConceptPl = 'Pojęcie początkowe';
-  static const goalConceptPl = 'Pojęcie końcowe';
-  static const startGamePl = 'Rozpocznij grę';
+  static const Map<String, Map<String, String>> translations = {
+    'en': {
+      'chaosThoughtFeast': 'Chaos Thought Feast',
+      'findYourLikings': 'Find Your Likings',
+      'likingSpectrumJourney': 'Liking Spectrum Journey',
+      'anyfinCanHappen': 'Anyfin Can Happen',
+      'profile': 'Profile',
+      'selectCategory': 'Select a category',
+      'startingConcept': 'Starting Concept',
+      'goalConcept': 'Goal Concept',
+      'startGame': 'Start Game',
+    },
+    'pl': {
+      'chaosThoughtFeast': 'Chaotyczna Uczta Myśli',
+      'findYourLikings': 'Znajdź Swoje Upodobania',
+      'likingSpectrumJourney': 'Podróż przez Spektrum Upodobań',
+      'anyfinCanHappen': 'Wszystko może się Zdarzyć',
+      'profile': 'Profil',
+      'selectCategory': 'Wybierz kategorię',
+      'startingConcept': 'Pojęcie początkowe',
+      'goalConcept': 'Pojęcie końcowe',
+      'startGame': 'Rozpocznij grę',
+    }
+  };
 
   static String getTranslatedString(String key, String language) {
-    switch (key) {
-      case 'chaosThoughtFeast':
-        return language == 'pl' ? chaosThoughtFeastPl : chaosThoughtFeast;
-      case 'findYourLikings':
-        return language == 'pl' ? findYourLikingsPl : findYourLikings;
-      case 'likingSpectrumJourney':
-        return language == 'pl' ? likingSpectrumJourneyPl : likingSpectrumJourney;
-      case 'anyfinCanHappen':
-        return language == 'pl' ? anyfinCanHappenPl : anyfinCanHappen;
-      case 'profile':
-        return language == 'pl' ? profilePl : profile;
-      case 'selectCategory':
-        return language == 'pl' ? selectCategoryPl : selectCategory;
-      case 'startingConcept':
-        return language == 'pl' ? startingConceptPl : startingConcept;
-      case 'goalConcept':
-        return language == 'pl' ? goalConceptPl : goalConcept;
-      case 'startGame':
-        return language == 'pl' ? startGamePl : startGame;
-      default:
-        return key;
-    }
+    return translations[language]?[key] ?? translations[defaultLanguage]![key] ?? key;
   }
 }
